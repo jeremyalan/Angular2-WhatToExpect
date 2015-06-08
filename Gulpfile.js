158f71e716ebfa39dcad6f09865e16d30dc9f1f5
Jeremy Mason <jeremy@artisantechgroup.com>
2015-06-08 09:08:07 -0500

    started adding Components section

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp');
+var path = require('path');
 var jade = require('gulp-jade');
+var less = require('gulp-less');
  
-gulp.task('jade', function() {
+gulp.task('compile', function() {
   gulp.src('./index.jade')
     .pipe(jade())
     .pipe(gulp.dest('.'));
+});
+
+gulp.task('less', function () {
+  return gulp.src('./css/custom.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./css'));
 });
